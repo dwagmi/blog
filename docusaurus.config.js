6d177b74b1511e54c8a3980d1e6fb44a0e232636
Kevin Kuang <200sxmeda@gmail.com>
2021-10-30 11:28:30 +1100

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://dwagmi.github.io',
+  baseUrl: '/dwagmi.github.io/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dwagmi', // Usually your GitHub org/user name.
+  projectName: 'dwagmi.github.io', // Usually your repo name.
 
   presets: [
     [
